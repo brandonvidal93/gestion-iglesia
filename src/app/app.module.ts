3409cc6e9d62aee591d6635cf1c0d4230d1bf267
brandon.vidal <brandonvidal93@gmail.com>
2023-12-01 09:57:52 -0500

    A - Route Home, Home C

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
+import { HomeComponent } from './feature/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/feature/asistencia/asistencia.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AsistenciaComponent } from './components/asistencia/asistencia.component';
+
+
+@NgModule({
+  declarations: [
+    AsistenciaComponent,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AsistenciaModule { }
